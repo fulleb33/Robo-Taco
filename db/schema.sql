6604bf71ad0fbf5364bd115aa5dea1c98a85ae55
Colin McDonnell <colindavidmcdonnell@gmail.com>
2017-08-11 15:22:48 -0500

    updates

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+-- Create a database
+CREATE DATABASE ingredients_db;
+USE ingredients_db;
+
+-- Create the table
+CREATE TABLE ingredients
+(
+id int NOT NULL AUTO_INCREMENT,
+item_name varchar(255) NOT NULL,
+type varchar(255) NOT NULL,
+price decimal(3,2) NOT NULL,
+date TIMESTAMP,
+PRIMARY KEY (id)
+);
